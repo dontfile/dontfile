1c7ba452f0edd2e6c5c89b1c6c7925efe14ad1c3
MatheusRich <matheusrichardt@gmail.com>
2018-04-08 14:46:13 -0300

    Creates page model tests

--- test/models/page_test.rb
+++ test/models/page_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class PageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @page = Page.create(
+      url: 'page_url',
+      content: 'Some text'
+    )
+  end
+
+  test "should save page" do
+    assert @page.save
+  end
+
+  test "should save page without any content" do
+    @page.content = ""
+    assert @page.save
+  end
+
+  test "should not save without url" do
+    @page.url = ""
+    assert_not @page.save
+  end
+
+  test "should not create page with an existing url" do
+    another_page = @page.dup
+    another_page.url = @page.url
+    @page.save
+
+    assert_not another_page.save
+  end
 end
