d45208da43f0c3aa122be791e1523570e478bd4d
MatheusRich <matheusrichardt@gmail.com>
2018-04-13 16:30:23 -0300

    Adds some route testing

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -3,32 +3,63 @@ require 'test_helper'
 class PagesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @page = Page.create(
-      url: "page_url",
-      content: "Some content"
+      url: 'page_url',
+      content: 'Some content'
     )
   end
 
-  test "should create page" do
+  test 'should route find to existing pages' do
+    assert_routing "/#{@page.url}",
+                   controller: 'pages',
+                   action: 'find',
+                   url: @page.url
+  end
+
+  test 'should route find to new pages' do
+    assert_routing '/new_page',
+                   controller: 'pages',
+                   action: 'find',
+                   url: 'new_page'
+  end
+
+  test 'should route update action' do
+    assert_routing(
+      {
+        method: 'patch',
+        path: "/#{@page.url}"
+      },
+      controller: 'pages',
+      action: 'update',
+      url: @page.url
+    )
+  end
+
+  test 'should create page' do
     assert_difference('Page.count') do
-      get "/some_page", params: { page: { content: "Some content", url: "some_page" } }
+      get '/some_page', params: {
+        page: {
+          content: 'Some content',
+          url: 'some_page'
+        }
+      }
     end
 
     assert_template :show
   end
 
-  test "should show page" do
+  test 'should show page' do
     get page_path(@page)
     assert_response :success
   end
 
   # This test isn't working, and I don't know why =(
-  # test "should update page" do
+  # test 'should update page' do
   #   old_content = @page.content
 
   #   patch "/#{@page.url}", params: {
   #     page: {
-  #       content: "New text",
-  #       url: @page.url
+  #       url: "/#{@page.url}",
+  #       content: 'New content'
   #     }
   #   }
   #   @page.reload
