4adc88c5f13f09bb02ce30eeea19d65b15325f6b
Ozmar Ugarte <ozmar_11@hotmail.com>
2018-10-09 12:16:17 -0500

    Change the file icon depending of file extension
    - Add the attachment_icon to the pages helper to render the icon of the file
    depending of the extension of the uploaded file

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,14 @@
 module PagesHelper
+  def attachment_icon(content_type)
+    icon_classes = case content_type
+    when /\/pdf/ then { style: "fa", name: "file-pdf" }
+    when /image\// then { style: "fa", name: "file-image" }
+    when /\/zip/ then { style: "fa", name: "file-archive" }
+    when /video\// then { style: "far", name: "file-video" }
+    else
+      { style: "fa", name: "file-alt" }
+   end
+
+    icon(icon_classes[:style], icon_classes[:name])
+  end
 end
