f23d7aa9419f4336024941294ee13359ca86b611
Ozmar Ugarte <ozmar_11@hotmail.com>
2018-10-09 15:31:15 -0500

    Address MatheusRich comments
    - Use the class far instead of fa in the icons
    - Use regular expression in the css to make the file icons match
    - Update the specs to the new icons' classes

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,12 +1,12 @@
 module PagesHelper
   def attachment_icon(content_type)
     icon_classes = case content_type
-                   when /\/pdf/ then { style: "fa", name: "file-pdf" }
-                   when /image\// then { style: "fa", name: "file-image" }
-                   when /\/zip/ then { style: "fa", name: "file-archive" }
+                   when /\/pdf/ then { style: "far", name: "file-pdf" }
+                   when /image\// then { style: "far", name: "file-image" }
+                   when /\/zip/ then { style: "far", name: "file-archive" }
                    when /video\// then { style: "far", name: "file-video" }
                    else
-                     { style: "fa", name: "file-alt" }
+                     { style: "far", name: "file-alt" }
     end
 
     icon(icon_classes[:style], icon_classes[:name])
--- test/helpers/pages_helper_test.rb
+++ test/helpers/pages_helper_test.rb
@@ -7,18 +7,18 @@ class PagesHelperTest < ActionView::TestCase
   end
 
   test "should get the icon of image when an image uploaded" do
-    assert_dom_equal(%{<i class="fa fa-file-image"></i>}, attachment_icon("image/jpeg"))
+    assert_dom_equal(%{<i class="far fa-file-image"></i>}, attachment_icon("image/jpeg"))
   end
 
   test "should get the icon of zip when zip file uploaded" do
-    assert_dom_equal(%{<i class="fa fa-file-archive"></i>}, attachment_icon("application/zip"))
+    assert_dom_equal(%{<i class="far fa-file-archive"></i>}, attachment_icon("application/zip"))
   end
 
   test "should get the icon of pdf when pdf file uploaded" do
-    assert_dom_equal(%{<i class="fa fa-file-pdf"></i>}, attachment_icon("application/pdf"))
+    assert_dom_equal(%{<i class="far fa-file-pdf"></i>}, attachment_icon("application/pdf"))
   end
 
   test "should get the default icon when another extension file uploaded" do
-    assert_dom_equal(%{<i class="fa fa-file-alt"></i>}, attachment_icon("application/csv"))
+    assert_dom_equal(%{<i class="far fa-file-alt"></i>}, attachment_icon("application/csv"))
   end
 end
